53e3e1cf49e81659c4c35d7ebff3514b0aa70136
hayato0309 <hayato.pluto.4538@gmail.com>
2021-08-29 21:18:11 +0900

    udemy until 46

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                <div>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Awesome post!" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 2:00AM" content="Amazing post!" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Yesterday at 5:00PM" content="Great post!" />
+            </ApprovalCard>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
